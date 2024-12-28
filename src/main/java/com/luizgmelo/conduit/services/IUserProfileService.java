6eaffffbbcba618bee8b018134947e175b44fd1e
Luiz Guilherme <88911920+luizgmelo@users.noreply.github.com>
2024-12-28 18:40:30 -0300

    refac: remove userProfile bad implementation

--- src/main/java/com/luizgmelo/conduit/services/IUserProfileService.java
+++
@@ -1,9 +0,0 @@
-package com.luizgmelo.conduit.services;
-
-import com.luizgmelo.conduit.models.UserProfile;
-
-public interface IUserProfileService {
-
-  UserProfile getProfile(String username);
-
-}
