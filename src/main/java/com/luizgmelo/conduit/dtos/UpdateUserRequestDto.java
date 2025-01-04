432417797d8d5fec57c89f5fb4ce26ce6b6085a8
Luiz Guilherme <88911920+luizgmelo@users.noreply.github.com>
2025-01-03 21:19:21 -0300

    fix: create validation for update user

--- src/main/java/com/luizgmelo/conduit/dtos/UpdateUserRequestDto.java
+++ src/main/java/com/luizgmelo/conduit/dtos/UpdateUserRequestDto.java
@@ -1,11 +1,14 @@
 package com.luizgmelo.conduit.dtos;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import org.hibernate.validator.constraints.URL;
 
 public record UpdateUserRequestDto(
-        @NotBlank(message = "Invalid email: must not be blank") String email,
-        @NotBlank(message = "Invalid username: must not be blank") String username,
-        @NotBlank(message = "Invalid password: must not be blank") String password,
-        @NotBlank(message = "Invalid image: must not be blank") String image,
-        @NotBlank(message = "Invalid bio: must not be blank") String bio) {
+        @NotBlank(message = "Invalid email: must not be blank") @Email String email,
+        @NotBlank(message = "Invalid username: must not be blank") @Size(min = 5, max = 30, message = "Username must between 5 and 30 characters") String username,
+        @NotBlank(message = "Invalid password: must not be blank") @Size(min = 8, max = 72, message = "Password must between 8 and 72 characters") String password,
+        @NotBlank(message = "Invalid image: must not be blank") @URL(message = "Image invalid url") String image,
+        @NotBlank(message = "Invalid bio: must not be blank") @Size(max = 150, message = "Bio must have less than 500 characters")  String bio) {
 }
