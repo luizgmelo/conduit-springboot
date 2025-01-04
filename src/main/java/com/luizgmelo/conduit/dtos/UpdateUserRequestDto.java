befbb7cf4e82a57572906e96885ca4304858ad81
Luiz Guilherme <88911920+luizgmelo@users.noreply.github.com>
2025-01-04 08:39:49 -0300

    fix: UpdateUserRequestDto add message email validation

--- src/main/java/com/luizgmelo/conduit/dtos/UpdateUserRequestDto.java
+++ src/main/java/com/luizgmelo/conduit/dtos/UpdateUserRequestDto.java
@@ -6,7 +6,7 @@ import jakarta.validation.constraints.Size;
 import org.hibernate.validator.constraints.URL;
 
 public record UpdateUserRequestDto(
-        @NotBlank(message = "Invalid email: must not be blank") @Email String email,
+        @NotBlank(message = "Invalid email: must not be blank") @Email(message = "Email is invalid format") String email,
         @NotBlank(message = "Invalid username: must not be blank") @Size(min = 5, max = 30, message = "Username must between 5 and 30 characters") String username,
         @NotBlank(message = "Invalid password: must not be blank") @Size(min = 8, max = 72, message = "Password must between 8 and 72 characters") String password,
         @NotBlank(message = "Invalid image: must not be blank") @URL(message = "Image invalid url") String image,
