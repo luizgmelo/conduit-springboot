8cfdb2c7f3f190fe644e086dca9a24d8a74bb424
Luiz Guilherme <luizgmelo64@gmail.com>
2025-07-04 18:14:25 -0300

    refac: tagController getTags endpoint

--- src/main/java/com/luizgmelo/conduit/controllers/TagController.java
+++ src/main/java/com/luizgmelo/conduit/controllers/TagController.java
@@ -2,13 +2,10 @@ package com.luizgmelo.conduit.controllers;
 
 import com.luizgmelo.conduit.dtos.TagResponseDTO;
 import com.luizgmelo.conduit.services.TagService;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/tags")
@@ -22,9 +19,6 @@ public class TagController {
 
     @GetMapping
     public ResponseEntity<TagResponseDTO> listTags() {
-        List<String> tagList = tagService.listTags();
-        TagResponseDTO response = new TagResponseDTO(tagList);
-        return ResponseEntity.status(HttpStatus.OK).body(response);
+        return ResponseEntity.ok(new TagResponseDTO(tagService.listTags()));
     }
-
 }
